61646aa5ae8bb414cfd876e382f5d22cc07ee790
FreeSoulCris <c.jimenez@witbooking.com>
2024-01-18 00:34:06 +0100

    js detail

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateQuote(event) {
   // Build the API URL
   let promptInput = document.querySelector("#user-prompts");
   let apiKey = "0239330ab540e803o5b4f9t7e63fbef4";
-  let prompt = `The user prompts are: Generate a great and potent motivational stoic (or not) quote about ${promptInput.value}`;
+  let prompt = `The user prompts are: Generate a short, great and potent motivational stoic (or not) quote about ${promptInput.value}`;
   let context =
     "You are an awesome and acclaimed life motivational coach who love and have the mission to help and inspire people with short but powerful quotes. Make sure to follow the user prompts to answer with the most accurate quote. Please respect the user language or idiom and respond in the same language as the user prompts. At the end of the quote, it's important to Sign it with the name of the real author.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
